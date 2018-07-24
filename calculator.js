201141e6f977c12db58515c6df5ebbe00c769bf5
gunturkh <guntur.kh@gmail.com>
2018-07-24 13:27:29 +0700

    Adding "cm2" on result from squareAreaDOM function

--- calculator.js
+++ calculator.js
@@ -8,6 +8,8 @@ const divideForm = document.getElementById("divide-form")
 const divideResult = document.getElementById("divide-result")
 const moduloForm = document.getElementById("modulo-form")
 const moduloResult = document.getElementById("modulo-result")
+const squareAreaForm = document.getElementById("square-area-form")
+const squareAreaResult = document.getElementById("square-area-result")
 
 // Basic Functions
 
@@ -16,6 +18,7 @@ const substract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 const modulo = (a, b) => a % b
+const squareArea = (a) => Math.pow(a, 2)
 
 // DOM Functions
 
@@ -55,9 +58,17 @@ const moduloDOM = (event) => {
     const moduloSecond = Number(document.getElementById("moduloVar2").value)
     if (moduloFirst && moduloSecond) moduloResult.innerHTML = modulo(moduloFirst, moduloSecond)
 }
+
+const squareAreaDOM = (event) => {
+    event.preventDefault()
+    const squareAreaValue = Number(document.getElementById("square-area-input").value)
+    squareAreaResult.innerHTML = squareArea(squareAreaValue) + " cm2"
+}
+
 // Event Listener
 addForm.addEventListener("submit", addDOM)
 substractForm.addEventListener("submit", substractDOM)
 multiplyForm.addEventListener("submit", multiplyDOM)
 divideForm.addEventListener("submit", divideDOM)
 moduloForm.addEventListener("submit", moduloDOM)
+squareAreaForm.addEventListener("submit", squareAreaDOM)
